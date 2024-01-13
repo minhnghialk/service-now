edd2a775a3703990ad5f2e01bc3e5d197c5d48b2
minhnghialk <nghia13031995@gmail.com>
2024-01-13 15:41:19 +0700

    Add logic sign in otp

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home/Home";
+import PhoneSignIn from "../pages/signin/PhoneSignIn";
+import SignInRole from "../pages/signin/SignInRole";
 
 const RouteSetup = () => {
   return (
@@ -7,6 +9,10 @@ const RouteSetup = () => {
       <Routes>
         {/* Home */}
         <Route path="/" element={<Home />}></Route>
+
+        {/* User */}
+        <Route path="signin" element={<PhoneSignIn />}></Route>
+        <Route path="signin/role" element={<SignInRole />}></Route>
       </Routes>
     </BrowserRouter>
   );
